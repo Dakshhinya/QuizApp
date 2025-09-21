7aca5e0fd00496acbb834d64ca6e9134abf47da4
Dakshinya Loganathan <dakshinya@ajirasoft.com>
2025-09-22 00:09:39 +0530

    dakku | signup & login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import { Navigate } from "react-router-dom";
+import StudentDashboard from "./pages/StudentDashboard";  
+import TeacherDashboard from "./pages/TeacherDashboard"; 
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/teacher-dashboard" element={<TeacherDashboard />} />    
       </Routes>
 
     </BrowserRouter>
--- frontend/src/component/LoginCard.jsx
+++ frontend/src/component/LoginCard.jsx
@@ -1,11 +1,48 @@
 
+import { useState } from "react";
 import LoginButton from "./LoginButton"
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 
 export default function LoginCard() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const users = [
+    { email: "student@example.com", password: "student123", role: "student" },
+    { email: "teacher@example.com", password: "teacher123", role: "teacher" },
+  ];
+
+
+   const handleLogin = (e) => {
+    e.preventDefault();
+
+    const user = users.find(
+      (u) => u.email === email && u.password === password
+    );
+
+    if (user) {
+     
+      localStorage.setItem("role", user.role);
+
+      if (user.role === "student") {
+        navigate("/student-dashboard");
+      } else if (user.role === "teacher") {
+        navigate("/teacher-dashboard");
+      }
+    } else {
+      setError("Invalid email or password");
+    }
+  };
+
   return (
     <>
     <div className="w-full max-w-md">
+       <form
+        onSubmit={handleLogin}
+        className="bg-white rounded-2xl shadow-2xl p-8 border border-gray-100"
+      >
       <div className="bg-white rounded-2xl shadow-2xl p-8 border border-gray-100">
         <div className="text-center mb-8">
           <div className="inline-flex items-center justify-center w-16 h-16 border-y-yellow-400 rounded-2xl mb-4">
@@ -17,13 +54,28 @@ export default function LoginCard() {
         <div className="space-y-6 flex flex-col">
           <div>
             <label className="text-sm font-medium text-gray-700 mb-2">Email</label>
-            <input className="px-4 py-3 border border-gray-300 rounded-lg  w-full ocus:ring-2 focus:ring-yellow-400 focus:border-yellow-400 transition-colors" type="email" name="email" /*value={} onChange={}*/ placeholder="Enter your email" required />
+            <input className="px-4 py-3 border border-gray-300 rounded-lg  w-full ocus:ring-2 focus:ring-yellow-400 focus:border-yellow-400 transition-colors" 
+            type="email" 
+            name="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+            placeholder="Enter your email" 
+            required />
           </div>
 
           <div>
             <label className="text-sm font-medium text-gray-700 mb-2">Password</label>
-            <input className="px-4 py-3 border border-gray-300 rounded-lg  w-full ocus:ring-2 focus:ring-yellow-400 focus:border-yellow-400 transition-colors" type="email" name="email" /*value={} onChange={}*/ placeholder="Enter your password" required />
+            <input className="px-4 py-3 border border-gray-300 rounded-lg  w-full ocus:ring-2 focus:ring-yellow-400 focus:border-yellow-400 transition-colors" 
+            type="password" 
+            name="password" 
+            value={password} 
+            onChange={(e)=> setPassword(e.target.value)} 
+            placeholder="Enter your password" 
+            required />
           </div>
+
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+
 
           <LoginButton/>
           
@@ -36,6 +88,7 @@ export default function LoginCard() {
 
         </div>
       </div>
+      </form>
     </div>
     
     </>
